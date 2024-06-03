75ba003797249c73c6038d89e9cc269cffc4f7c6
LordNinth <rr621801@gmail.com>
2024-06-03 21:57:59 +0100

    views

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,10 @@ const router = express.Router();
 const User = require("../model/author");
 const Book = require("../model/book");
 
-// router.get("/index", async (req, res, next) => {
-//   // const user =  new User({ user: "user1", age: 23, book: '665a2ecd6c3971e437e13914'});
+router.get("/index", async (req, res, next) => {
 
-//   // await user.save();
-
-//   // const book =  new Book({title:"jungle", user: "665a32435648421704d21803"})
-//   // await book.save();
-//   const find = await Book.find({ user: "665a32435648421704d21803" });
-//   console.log(find);
-
-//   res.render("index", { title: "Express" });
-// });
+  res.render("index", { title: "Express" });
+});
 
 // GET home page.
 router.get("/", function (req, res) {
